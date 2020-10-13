93ba2eb4ea7f7c652068f40676b0be7121f8d6b0
jeferson <jeferson.santos@ilog.com.br>
2020-10-13 20:47:00 -0300

    Styled Components

---
+++ src/Pages/Dashboard/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+// Template literals
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 56px;
+`;
