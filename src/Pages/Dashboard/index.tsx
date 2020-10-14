68e579fe00446b181f54bfd851a351cfe9ca14f8
jeferson <jeferson.santos@ilog.com.br>
2020-10-13 21:36:01 -0300

    Input And Button

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logodash.svg';
+
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+    </>
+  );
 };
 
 export default Dashboard;
--- src/Pages/Dashboard/styles.ts
+++ src/Pages/Dashboard/styles.ts
@@ -1,12 +1,42 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 // Template literals
 
 export const Title = styled.h1`
-  font-size: 48px;
+  font-size: 3rem;
   color: #3a3a3a;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: bold;
-  line-height: 56px;
+  margin-top: 5rem;
+  max-width: 28.62rem;
+  line-height: 3.5rem;
+`;
+
+export const Form = styled.form`
+  margin-top: 2.5rem;
+  max-width: 43.75rem;
+  display: flex;
+  input {
+    flex: 1;
+    height: 4.375rem;
+    padding: 0 1.5rem;
+    border: 0;
+    border-radius: 0.313rem 0 0 0.313rem;
+    color: #3a3a3a;
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+  button {
+    width: 13.125rem;
+    height: 4.375rem;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+    &:hover {
+      background: ${shade(0.3, '#04d361')};
+    }
+  }
 `;
